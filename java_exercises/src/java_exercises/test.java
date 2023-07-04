d0bf2fcc379672df5bf032c4b414dd6c3ea1188d
Anthony Moises Condori Choccata <113246456+Alt-Anthony@users.noreply.github.com>
2023-07-04 10:41:02 -0500

    second exercise

--- java_exercises/src/java_exercises/test.java
+++ java_exercises/src/java_exercises/test.java
@@ -3,8 +3,19 @@ package java_exercises;
 public class test {
 
 	public static void main(String[] args) {
-			Person person = new Person(12,"alpha");
-			System.out.println(person.getAge()+person.getName());
+			//Person person = new Person(12,"alpha");
+			//System.out.println(person.getAge()+person.getName());
+		Dog dog = new Dog("beta","1");
+		Dog dog2 = new Dog("omega","2");
+		System.out.println(dog.getName()+", "+dog.getBreed());
+		System.out.println(dog2.getName()+", "+dog2.getBreed());
+		dog.setName("beta2");
+		dog.setBreed("2");
+		dog2.setName("omega2");
+		dog2.setBreed("3");
+		System.out.println(dog.getName()+", "+dog.getBreed());
+		System.out.println(dog2.getName()+", "+dog2.getBreed());
+			
 
 	}
 
